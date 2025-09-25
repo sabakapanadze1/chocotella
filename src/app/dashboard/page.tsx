e12fac7b45dfc4395e0be997073b11fc53596b2f
sabakapanadze2 <skapanadze@companysage.com>
2025-09-25 19:55:12 +0400

    add routing and fetch data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,10 +3,25 @@ import Box from "@mui/material/Box";
 import Gela from "@/part/test";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Products() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+  if (!response.ok) throw new Error("Fail Fetch");
+  const albums = await response.json();
+  console.log(albums, "alaklkalk");
+
   return (
     <div>
       <Link href={"dashboard/products"}>products</Link>
+      ssll
+      {albums.map((album: { id: number; name: string }) => {
+        return (
+          <div className="flex justify-between" key={album.id}>
+            <h3 className="color-black">Album name:{album.name}</h3>
+            <p>albom id:{album.id}</p>
+          </div>
+        );
+      })}
+      <h3>alboms</h3>
     </div>
   );
 }
